b2610d544f11bfd0444d8788652523899f146d3f
Andrew <acbuckaroo@gmail.com>
2020-04-01 17:11:45 -0400

    Added Config File Parser

--- src/Packer/src/pakman.cpp
+++ src/Packer/src/pakman.cpp
@@ -87,3 +87,18 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 }
+
+//Grab Configuration Options
+config_option_t co;
+    if ((co = read_config_file("/Users/your_path_to_config_file/example.conf")) == NULL) {
+        perror("read_config_file()");
+        return -1;
+    }
+    while(1) {
+        printf("Key: %s\nValue: %s\n", co->key, co->value);
+        if (co->prev != NULL) {
+            co = co->prev;
+        } else {
+            break;
+        }
+    }
