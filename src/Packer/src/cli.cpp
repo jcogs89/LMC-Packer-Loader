3c802a6d8e5af67b2e2ed1a0b13b24e5de7dc974
mpalmer7 <mitchpalmer12@gmail.com>
2020-04-02 20:43:42 -0400

    refactored cli and cliopts

--- src/Packer/src/cli.cpp
+++ src/Packer/src/cli.cpp
@@ -5,40 +5,31 @@
 
 using namespace std;
 
-static const char options[] = "\
-\n \e[1;32mChoose an option:\e[0;17m \n\
+static const char options[] = "\n \e[1;32m\
+Choose an option: \e[0;17m \n\
 1. List Available Payloads \n\
 2. Add new payload \n\
 3. Send payload \n\
 0. Exit \n\
 \e[1;32m>\e[0;17m ";
 	
-int cli(vector<string> files, string pathpacked, string pathstaging)
-{
+
+int cli(vector<string> files, string pathpacked, string pathstaging) {
 		int id;
-		while (1)
-		{
+
+		while (1) {
 			cout << options;
 			cin >> id;
-			if(id==1)
-			{
+			if(id==1) {
 				listpayloads(files);
-			}
-			else if(id==2)
-			{
+			} else if(id==2) {
 				addpayload(pathpacked, pathstaging);
-			}
-			else if(id==3)
-			{
+			} else if(id==3) {
 				sendpayload(pathpacked);
-			}
-			else if(id==0)
-			{
+			} else if(id==0) {
 				cout << "\nexit\n";
 				break;
-			}
-			else
-			{
+			} else {
 				cout << "Invalid Option! Please enter a number.\n";
 			}
 		}
--- src/Packer/src/cliopts.cpp
+++ src/Packer/src/cliopts.cpp
@@ -4,6 +4,7 @@
  *  Created on: Feb 27, 2020
  *      Author: cbai
  */
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -13,49 +14,50 @@
 #include "sshwraper.h"
 using namespace std;
 
-void listpayloads(vector<string> files)
-{
+
+void listpayloads(vector<string> files) {
 	printf("current payloads\n\n");
-	//2 or 11 is magic, trust me
+	//2 or 11 is magic, trust me  //ToDo, whoever wrote this - please clarify.
 	dirprint(files,11);
 }
 
-void addpayload(string pathpacked, string pathstaging)
-{
+
+int addpayload(string pathpacked, string pathstaging) {
 	char y = 'y';
 	char yc = 'Y';
 	char n = 'n';
 	char ans;
-
-	printf("The following payloads are availible in the staging folder:");
-	vector<string> stage = dirlist(pathstaging);
-	printf("\n");
-
-
-	//ToDo add way to back out gracefully
+	string inp;
 	unsigned int id;
-	while (1)
-	{
-		//10 is magic, trust me
-		dirprint(stage, 10);
-		printf("\nEnter number for file to be packed.\n>> ");
-		id = intinput();
+	vector<string> stage = dirlist(pathstaging);
 
-		printf("%d", id);
-		if (id>stage.size()-1)
-		{
-			cout << "Invalid Option\n\n";
-			continue;
-		}
-			else
-		{
-			//cout << stage[id];
-			printf("\n%i\n",id);
-			break;
+	printf("The following payloads are availible in the staging folder:\n");
+		//10 is magic, trust me  //ToDo, whoever wrote this - please clarify.
+		dirprint(stage, 10);
+		printf("\nEnter the number for file to be packed ('x' to back out):");
+
+	while (1) {
+		printf("\n>> ");
+		cin >> inp;
+
+		try {
+			id = std::stoi(inp);
+			if (id>stage.size()-1) {
+				cout << "Invalid option, please try again.";
+				continue;
+			} else {
+				printf("\n%i\n",id);
+				break;
+			}
+		} catch (...) {
+			if ((inp != "x") and (inp != "X")) {
+				printf("Unrecognized input, please try again.");
+			} else {
+				return 0;
+			}
 		}
 	}
 	cout <<"File :\"" << stage[id] << "\" selected";
-
 
 	// COMPRESSION <><>
 	string compression_outp= "./Payloads/"+stage[id].substr(10)+".zips";
@@ -63,40 +65,30 @@ void addpayload(string pathpacked, string pathstaging)
 	ziphelp(iput, compression_outp);
 	//ret = uziphelp(outp, outp+".txt");
 
-
 	//ENCRYPTION <><>
 	string encryption_outp= "./Payloads/"+stage[id].substr(10)+".encr";
 	encrypthelp(compression_outp, encryption_outp);
 
-
-	while (1)
-		{
-			printf("Do you want to delete the source in staging (y/n)?\n>> ");
-			cin >> ans;
-			if (ans==y or ans == yc)
-			{
-				if( remove( iput.c_str() ) != 0 )
-				    perror( "Error deleting file" );
-				  else
-				    puts( "File successfully deleted" );
-				break;
-			}
-			else if (ans==n)
-			{
-				break;
-			}
-			else
-			{
-				cout << "Invalid Option\n";
-			}
-
+	while (1) {
+		printf("Do you want to delete the source in staging (y/n)?\n>> ");
+		cin >> ans;
+		if (ans==y or ans == yc) {
+			if( remove( iput.c_str() ) != 0 )
+				perror( "Error deleting file" );
+			  else
+				puts( "File successfully deleted" );
+			break;
+		} else if (ans==n) {
+			break;
+		} else {
+			cout << "Invalid Option\n";
 		}
+	}
 }
 
 
-void sendpayload(string pathpacked)
-{
+void sendpayload(string pathpacked) {
 	printf("\nsend a payload\n");
-	//logic to select server here <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	//ToDo logic to select server here <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	connect("127.0.0.1");
 }
