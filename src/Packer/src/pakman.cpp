a7d9148c6ef2d60116f1380df158155ebe15b2ab
mpalmer7 <mitchpalmer12@gmail.com>
2020-04-09 17:58:42 -0400

    packman refactor

--- src/Packer/src/pakman.cpp
+++ src/Packer/src/pakman.cpp
@@ -1,22 +1,22 @@
+//requires c++ v17  //ToDo reconfirm this is true
+//-std=c++17
+
 #include <stdio.h>
 #include <unistd.h>
 #include <vector>
 #include <string.h>
 #include <iostream>
-//#include "testing.h"
+
 #include "dirlist.h"
 #include "cli.h"
 #include "config_parser.h"
 #include "incom.h"
+//#include "testing.h"
 
 using namespace std;
 
-//requires c++ v17
-//-std=c++17
-
-
 static const char usage[] = "\n\
-Usage: packman [OPTIONS]... TARGET [HOSTNAME:PORT] PAYLOAD [FILE]\n\
+Usage: packman [OPTIONS]... TARGET [HOSTNAME:PORT] PAYLOAD [FILE] \n\
 Sends a user defined PAYLOAD to TARGET. \n\
 Example: packman 192.168.0.1:1337 ~/payload.exe \n\n\
 	-h, --help\tPrints this Help Page\n\
@@ -24,41 +24,34 @@ Example: packman 192.168.0.1:1337 ~/payload.exe \n\n\
 	\n\n";
 
 
-int forking ()
-{
-	printf("\nstart fork\n");
+int forking () {
 	pid_t parent = getpid();
 	pid_t pid = fork();
-	if (pid == 0)
-	{
+
+	printf("\nstart fork\n");
+
+	if (pid > 0) { // only good case
+	   // parent process
+	   printf("main process");
+	   return 1;
+
+	} else if (pid == 0) {
 		// child process
 	    printf("child process");
-	    incom("knownhosts", parent);
+	    incom("knownhosts", parent);  //main of incom.cpp
 	    printf("child died");
-	    exit(0);
-	    return 0;
-	}
-	else if (pid > 0)
-	{
-	// parent process
-	    printf("main process");
-		return 1;
-	}
-	else
-	{
-	// fork failed
-	printf("\nfork() failed!\n");
-	exit(0);
-	return(-69);
+
+	} else {
+		// fork failed
+		printf("\nfork() failed!\n");
 	}
 	exit(0);
-	return(-69);
+	return(0);
 }
 
 
 int main(int argc, char *argv[])
 {
-	printf("starting");
 	int option;
 	char tvalue[32];
 	char fvalue[32];
@@ -66,20 +59,21 @@ int main(int argc, char *argv[])
 	std::string pathpacked;
 	std::string pathstaging;
 
-	ConfigFile cfg("./packer.conf");
+	ConfigFile cfg("./packer.conf"); //ToDo Add default conf file and option to pass CLA of its path
 
+	//Read Config for required fields
 	if (cfg.keyExists("payloads_dir")) {
 		pathpacked = cfg.getValueOfKey<std::string>("payloads_dir");
 		//printf("Config worked: %s\n", pathpacked.c_str());
 	} else {
-		printf("No payloads directory specified in config.  Please update 'payloads_dir' in the config.");
+		printf("No payloads directory specified in config.  Please update 'payloads_dir' in the config."); //ToDo or ask user to input now
 		exit(0);
 	}
 	if (cfg.keyExists("staging_dir")) {
 		pathstaging = cfg.getValueOfKey<std::string>("staging_dir");
 		//printf("Config worked: %s\n", pathstaging.c_str());
 	} else {
-		printf("No staging directory specified in config.  Please update 'staging_dir' in the config.");
+		printf("No staging directory specified in config.  Please update 'staging_dir' in the config."); //ToDo or ask user to input now
 		exit(0);
 	}
 
@@ -87,21 +81,18 @@ int main(int argc, char *argv[])
 		files = dirlist(pathpacked);
 	}
 	catch (...) {
-		printf("Payloads dir not found");
+		printf("Payloads dir (%s) not found", pathpacked.c_str());
 		exit(0);
 	}
 
-	bool exists = cfg.keyExists("port");
-	std::cout << "port: " << std::boolalpha << exists << "\n";
-
-	std::string portValue = cfg.getValueOfKey<std::string>("port");
-	std::cout << "value of key port: " << portValue << "\n";
-
-
-	//dirprint(files);
+	//READING FROM CONFIG FILE DEBUG
+	//bool exists = cfg.keyExists("port");
+	//std::cout << "port: " << std::boolalpha << exists << "\n";
+	//std::string portValue = cfg.getValueOfKey<std::string>("port");
+	//std::cout << "value of key port: " << portValue << "\n";
 
 	//program requires one argument
-	//ToDo do we want to feed more arguments?  No arguments (if using config)?
+	//ToDo do we want to feed more arguments?  Or feed no arguments? (if using config)
 	if(argc<2) {
 		printf(usage);
 		return 0;
@@ -121,17 +112,15 @@ int main(int argc, char *argv[])
 			case ':':
 				printf("\nOption %c needs a value\n", optopt);
 				break;
-			case '?': //used for some unknown options
+			case '?':
 				printf("\nUnknown option: %c\n", optopt);
 				break;
 			case 'h':
 				printf(usage);
-				//testing();
 				break;
 			case 's':
-				printf("Starting\n");
-				forking(); //ToDo this was broken, who last pushed??
-				cli(files, pathpacked, pathstaging);
+				forking(); //ToDo Carl - what does forking to? Descriptive naming...
+				cli(files, pathpacked, pathstaging); //main execution
 				return 1;
 				break;
 			return 0;
