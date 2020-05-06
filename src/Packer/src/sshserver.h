08a6ee02b48613a5d10845f4156146c06ba5393c
Andrew <acbuckaroo@gmail.com>
2020-05-06 03:47:34 -0400

    Gitignore Fix

--- src/Packer/src/sshserver.h
+++
@@ -1,8 +0,0 @@
-/*
- * sshserver.h
- *
- *  Created on: Apr 1, 2020
- *      Author: cbai
- */
-#include <string>
-int sshserver(int portp, std::string ssh_host_dsa_key, std::string ssh_host_rsa_key );
