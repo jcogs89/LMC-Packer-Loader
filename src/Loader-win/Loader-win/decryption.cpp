3a7ef4e883e6061ebb73f85cbf2465294998fcdf
aherrera <andrevin1298@gmail.com>
2020-05-06 18:48:35 -0400

    Decyrption on Loader

--- src/Loader-win/Loader-win/decryption.cpp
+++ src/Loader-win/Loader-win/decryption.cpp
@@ -1,14 +1,36 @@
-// decfile.cpp : Defines the entry point for the console application.
-//
-#include "windows.h"
-// ToDo Allison recommends using bcrypt instead of wincrypt.
-#include "wincrypt.h"
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
 
-void LogFile(unsigned char* lpBuffer, DWORD buflen, LPCSTR fname);
+#include "crypto/modes.h"
+#include "crypto/aes.h"
+#include "crypto/filters.h"
 
-int decryption()
-{
-	printf("Todo- implement decryption");
-	return 0;
+
+std::string Decrypt(std::string cipher_in, CryptoPP::byte key[CryptoPP::AES::DEFAULT_KEYLENGTH], CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE]) {
+
+
+    std::string ciphertext = cipher_in;
+    std::string decryptedtext;
+
+    //
+    // Decrypt
+    //
+    CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
+
+    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decryptedtext));
+    stfDecryptor.Put(reinterpret_cast<const unsigned char*>(ciphertext.c_str()), ciphertext.size());
+    stfDecryptor.MessageEnd();
+
+    //
+    // Dump Decrypted Text
+    //
+    std::cout << "Decrypted Text: " << std::endl;
+    std::cout << decryptedtext;
+    std::cout << std::endl << std::endl;
+
+    return decryptedtext;
 }
+
+//Decryption call
+//string decryption_outp = Decrypt(encryption_outp, key, iv)
