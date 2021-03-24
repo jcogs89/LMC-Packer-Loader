901c54304682bfc035b8ca90e53cf55f81741f41
jcogs89 <63980994+jcogs89@users.noreply.github.com>
2021-03-23 21:21:30 -0400

    add decrypt function

--- src/Loader-win/Loader-win/decryption.cpp
+++ src/Loader-win/Loader-win/decryption.cpp
@@ -7,8 +7,45 @@
 
 void LogFile(unsigned char* lpBuffer, DWORD buflen, LPCSTR fname);
 
-int decryption()
+typedef struct payloadStruct
 {
-	printf("Todo- implement decryption");
-	return 0;
+    unsigned int uncompressedLength;
+    unsigned int encryptedLength;
+    unsigned int decryptedLength;
+    char** argv;
+    char** envp;
+} PayloadStruct;
+
+unsigned char* decrypt(unsigned char* encrypted, unsigned int input_length, unsigned int original_size, unsigned char* key)
+{
+    unsigned char encrypted_msg[input_length - 24];
+    unsigned char nonce[24];
+
+    const unsigned int ciphertext_len = original_size + 16;
+
+    unsigned char* decrypted = calloc(original_size, sizeof(unsigned char));
+
+    if (sodium_init() == -1)
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < input_length; i++)
+    {
+        if (i > 23)
+        {
+            encrypted_msg[i - 24] = encrypted[i];
+        }
+        else
+        {
+            nonce[i] = encrypted[i];
+        }
+    }
+
+    if (crypto_secretbox_open_easy(decrypted, encrypted_msg, ciphertext_len, nonce, key) != 0)
+    {
+        return NULL;
+    }
+
+    return decrypted;
 }
--- src/Loader-win/Loader-win/decryption.h
+++ src/Loader-win/Loader-win/decryption.h
@@ -1,3 +1,6 @@
-#pragma once
-int decryption();
+#include <sodium.h>
+#include <stdio.h>
+#include <string.h>
+
+unsigned char* decrypt(unsigned char* encrypted, unsigned int input_length, unsigned int original_size, unsigned char* key);
 #pragma once
